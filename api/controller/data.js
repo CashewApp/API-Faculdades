2d8689cf64af1b90910867a3de22221ed5e6fdda
HenriqueDelben <hd.delben@gmail.com>
2023-09-18 13:50:12 -0300

    :sparkles: feat: Add support to pquery universities by its name

--- api/controller/data.js
+++ api/controller/data.js
@@ -2,7 +2,29 @@ module.exports = () => {
     const dataFaculDB = require('../data.json');
     const controller = {};
 
-    controller.listDataFacul = (req, res) => res.status(200).json(dataFaculDB)
+    // Endpoint to filter data based on query parameter 'nome'
+    controller.filterDataByName = (req, res) => {
+        const { nome } = req.query;
+        
+        if (!nome) {
+            return res.status(400).json({ error: 'Query parameter "nome" is required' });
+        }
+        
+        const normalizedQuery = nome.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+        const regex = new RegExp(normalizedQuery, 'i');
 
-    return controller
-}
+        const filteredData = dataFaculDB.filter(item => {
+            // Remove accents from item.nome and make it case-insensitive
+            const normalizedNome = item.nome.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+            return regex.test(normalizedNome);
+        });
+        
+        if (filteredData.length === 0) {
+            return res.status(404).json({ error: 'No matching data found' });
+        }
+
+        return res.status(200).json(filteredData);
+    };
+
+    return controller;
+};
--- api/routes/dataRoutes.js
+++ api/routes/dataRoutes.js
@@ -1,6 +1,10 @@
 module.exports = app => {
     const controller = require('../controller/data')();
     
+    /*app.route('/api/v1/faculdades')
+        .get(controller.listDataFacul);*/
+
+    // Add a new route to filter data by 'nome' query parameter
     app.route('/api/v1/faculdades')
-        .get(controller.listDataFacul)
-}
+        .get(controller.filterDataByName); // Use this endpoint to filter by 'nome'
+};
